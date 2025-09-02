bb197ba06623f6808f87b7196d9d9b8eb4d5fbf4
Arthur <arthur.g2006@aluno.ifsc.edu.br>
2025-09-02 10:26:52 -0300

    pilha vazia

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -3,3 +3,10 @@
 void criar_pilha(Pilha *p) {
     p->topo = -1;
 }
+int pilha_vazia(Pilha *p) {
+    if (p->topo == -1) {
+        return 1; //se for verdadeiro, ela tá vazia
+    }
+    return 0; //falso ela não está vazia
+}
+
