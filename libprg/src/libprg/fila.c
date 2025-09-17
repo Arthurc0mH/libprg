cc04e36e11c0a21fa46c7711237e54684c67b904
Arthur <arthurgraciliano2006@gmail.com>
2025-09-16 22:37:10 -0300

    fila

---
+++ libprg/src/libprg/fila.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "libprg/libprg.h"
+
+void criar_fila(Fila *f) {
+    f->inicio = 0;
+    f->fim = 0;
+    f->tamanho = 0;
+}
+
+int fila_vazia(Fila *f) {
+    return (f->tamanho == 0);
+}
+
+int fila_cheia(Fila *f) {
+    return (f->tamanho == MAX_FILA);
+}
+
+int enfileirar(Fila *f, int valor) {
+    if (fila_cheia(f)) {
+        return 0;
+    }
+    f->dados[f->fim] = valor;
+    f->fim = (f->fim + 1) % MAX_FILA;
+    f->tamanho++;
+    return 1;
+}
+
+int desenfileirar(Fila *f, int *valor_removido) {
+    if (fila_vazia(f)) {
+        return 0;
+    }
+    *valor_removido = f->dados[f->inicio];
+    f->inicio = (f->inicio + 1) % MAX_FILA;
+    f->tamanho--;
+    return 1;
+}
+
+int consultar_frente(Fila *f, int *valor_frente) {
+    if (fila_vazia(f)) {
+        return 0;
+    }
+    *valor_frente = f->dados[f->inicio];
+    return 1;
+}
+
+int tamanho_fila(Fila *f) {
+    return f->tamanho;
+}
