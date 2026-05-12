2ea36f2922067d3e35f443be87fcf6831aa919aa
arthur <arthurgraciliano2006@gmail.com>
2026-05-12 17:10:34 -0300

    bubble e merge sort

---
+++ libprg/src/libprg/ordena.c
@@ -0,0 +1,52 @@
+#include "libprg/libprg.h"
+
+int* merge(int* vetor, int esquerda, int meio, int direita);
+
+int* bubble_sort(int* vetor, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                int aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+    }
+    return vetor;
+}
+
+int* merge_sort(int* vetor, int esquerda, int direita) {
+    if (esquerda < direita) {
+        int meio = esquerda + (direita - esquerda) / 2;
+        merge_sort(vetor, esquerda, meio);
+        merge_sort(vetor, meio + 1, direita);
+        merge(vetor, esquerda, meio, direita);
+    }
+    return vetor;
+}
+
+int* merge(int* vetor, int esquerda, int meio, int direita) {
+    int aux[direita - esquerda + 1];
+    int i = esquerda, j = meio + 1, k = 0;
+    while (i <= meio && j <= direita) {
+        if (vetor[i] <= vetor[j]) {
+            aux[k] = vetor[i];
+            i = i + 1;
+        }else {
+            aux[k] = vetor[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+    while (i <= meio) {
+        aux[k] = vetor[i];
+        i = i + 1; k = k + 1;
+    }
+    while (j <= direita) {
+        aux[k] = vetor[j];
+        j = j + 1; k = k + 1;
+    }
+    for (i = esquerda; i <= direita; i++) {
+        vetor[i] = aux[i - esquerda];
+    }
+}
